5a2170b9f5ca1314947a4aac7bfd6eb7dd0d8d27
Niels Kloster Andreassen <nika@telenor.dk>
2014-09-11 13:47:51 +0200

    Basic login html

--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,8 @@
 var clientDemo = angular.module('ClientDemo', []);
 
 function mainController($scope, $http) {
+
+	$scope.login = function(){
+		console.log("HEJ!" + $scope.formData.customerId);
+	};
 }
--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require("express");
 var logfmt = require("logfmt");
 
+var login = require('./api/login.js');
+
 var app = express();
 app.use(logfmt.requestLogger());
 app.use(express.bodyParser());
 app.use(express.favicon());
-
 app.use(express.static(__dirname + '/public'));
-app.get('/login', function(req, res){
-});
+
+app.post('/login', login.create);
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
