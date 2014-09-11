48adaa41dc4e9dd9ccc10c08af33e5e27b81427d
Niels Kloster Andreassen <nika@telenor.dk>
2014-09-11 14:25:54 +0200

    Implemented hardcoded authentication

--- api/login.js
+++ api/login.js
@@ -1,3 +1,10 @@
 exports.create = function(req, res) {
-	res.send('hello');
+	if (req.body.customerId === "123" && req.body.password === "123")
+	{
+		res.send('hello');
+	}
+	else {
+		res.statusCode = 401;
+    	res.send("Unauthorized!");
+	}
 };
--- public/js/client.js
+++ public/js/client.js
@@ -1,8 +1,13 @@
 var clientDemo = angular.module('ClientDemo', []);
 
 function mainController($scope, $http) {
-
 	$scope.login = function(){
-		console.log("HEJ!" + $scope.formData.customerId);
+		$http.post('/login', $scope.formData)
+			.success(function(data) {
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
 	};
 }
