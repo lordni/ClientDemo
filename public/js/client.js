f6342d22a77f05a34d0fa460cd7873679831ede5
Niels Kloster Andreassen <nika@telenor.dk>
2014-09-11 14:36:59 +0200

    Show password failue in ui

--- public/js/client.js
+++ public/js/client.js
@@ -4,9 +4,11 @@ function mainController($scope, $http) {
 	$scope.login = function(){
 		$http.post('/login', $scope.formData)
 			.success(function(data) {
+				$scope.failingPassword = false;
 				console.log(data);
 			})
 			.error(function(data) {
+				$scope.failingPassword = true;
 				console.log('Error: ' + data);
 			});
 	};
