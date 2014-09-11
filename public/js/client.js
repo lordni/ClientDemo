b28d0580e55d1e297b8e2b2f3d423b25921fb14a
Niels Kloster Andreassen <nika@telenor.dk>
2014-09-11 14:59:48 +0200

    Setup angular routing

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,26 @@
-var clientDemo = angular.module('ClientDemo', []);
+'use strict';
 
-function mainController($scope, $http) {
+angular.module('ClientDemo', ['ngRoute'])
+
+.config(function ($routeProvider, $locationProvider) {
+	$routeProvider
+      .when('/', {
+        templateUrl: 'templates/main.html',
+        controller: 'mainController'
+      })
+      .when('/min-side', {
+        templateUrl: 'templates/min-side.html',
+        controller: 'authenticatedController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+	$locationProvider.html5Mode(false);
+})
+
+.controller('mainController', function($scope, $http) {
+	console.log('starting mainController');
 	$scope.login = function(){
 		$http.post('/login', $scope.formData)
 			.success(function(data) {
@@ -12,4 +32,8 @@ function mainController($scope, $http) {
 				console.log('Error: ' + data);
 			});
 	};
-}
+})
+
+.controller('authenticatedController', function($scope) {
+	console.log('starting authenticatedController');
+});
