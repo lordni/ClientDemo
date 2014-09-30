702d328400fa80cbc427fc914ac8485537a56114
Thomas Lundström <thlu@telenor.dk>
2014-09-30 09:47:03 +0200

    Move mainController to the main file

--- public/js/client.js
+++ public/js/client.js
@@ -33,36 +33,6 @@ angular.module('ClientDemo', [
 	$locationProvider.html5Mode(false);
 })
 
-.controller('mainController', function($scope, $http, $location, mainModule, rx) {
-	console.log('starting mainController');
-
-	var extractNewValue = function (data) {
-		return (data.newValue === undefined ? '' : data.newValue);
-	}
-
-	var customerIdStream = $scope.$toObservable('formData.customerId').map(extractNewValue);
-	var passwordStream = $scope.$toObservable('formData.password').map(extractNewValue);
-	var loginButtonStream = $scope.$createObservableFunction('login');
-
-	var viewModel = mainModule.createViewModel(customerIdStream, passwordStream);
-
-	loginButtonStream.subscribe(function () {
-		viewModel.login()
-		.subscribe(function(data) {
-			console.log("login complete");
-			console.log(data);
-
-			$scope.failingPassword = false;
-			$location.path("/min-side");
-		}, function (err) {
-			console.log("login error");
-			console.log(err);
-
-			$scope.failingPassword = true;
-		});
-	});
-})
-
 .controller('authenticatedController', function($scope, $http, authenticatedModule, rx) {
 	$scope.balanceText = 'Loading...';
 
