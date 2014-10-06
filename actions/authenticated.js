51c01a19fe3269e5bd869edb8e6045b409330886
Thomas Lundström <thlu@telenor.dk>
2014-10-06 14:37:27 +0200

    Made login required for /balance

--- actions/authenticated.js
+++ actions/authenticated.js
@@ -4,19 +4,27 @@
 	var api = require('../api/account.js');
 
 	exports.balance = function (req, res) {
-		api.balance('42439273')
-			.then(function (data) {
-				console.log('data: ');
-				console.log(data);
-				res.send({
-					balance: data.balance
-				});
-			}, function(err) {
-				console.log('error:');
-				console.log(err);
 
-				res.statusCode = 500;
-				res.send(err);
-			});
+		if (!req.isAuthenticated()) {
+			console.log("un-authenticated user");
+			res.statusCode = 401;
+			res.send("Unauthenticated user. Please sign in");
+		} else {
+			var dummyPhoneNumber = '42439273';
+			api.balance(dummyPhoneNumber)
+				.then(function (data) {
+					console.log('data: ');
+					console.log(data);
+					res.send({
+						balance: data.balance
+					});
+				}, function(err) {
+					console.log('error:');
+					console.log(err);
+
+					res.statusCode = 500;
+					res.send(err);
+				});
+		}
 	}
 })();
