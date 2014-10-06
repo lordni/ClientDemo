54835080826c847cae70ca969cc75956cf9bdd96
Thomas Lundström <thlu@telenor.dk>
2014-10-06 13:49:25 +0200

    Split core and angular for signup module

--- public/js/client/signup/signup.js
+++ public/js/client/signup/signup.js
@@ -1,7 +1,8 @@
+'use strict';
+
+var app = app || {};
+
 (function (){
-
-	var app = app || {}
-
 	app.signupModule = {
 		create: function (http) {
 			return {
@@ -87,44 +88,4 @@
 			};
 		}
 	};
-
-	angular.module('ClientDemo.Signup', ['ClientDemo.Product', 'rx']).
-		factory('signupModule', function ($http) {
-			var module = app.signupModule.create($http);
-
-			return module;
-		}).
-		controller('signupController', function ($scope, $routeParams, rx, productModule, signupModule, observeOnScope) {
-			var product = productModule.mapProduct($routeParams.productName);
-
-			var simCardSelectionStream = observeOnScope($scope, 'simtype');
-			var npOrNewNumberStream = observeOnScope($scope, 'npOrNewNumber');
-			var accountTypeStream = observeOnScope($scope, 'accountTypeSelection');
-
-			var nameStream = observeOnScope($scope, 'name');
-			var emailStream = observeOnScope($scope, 'email');
-			var emailConfirmationStream = observeOnScope($scope, 'emailConfirmation');
-
-			var viewModel = signupModule.createViewModel(
-				product,
-				simCardSelectionStream,
-				npOrNewNumberStream,
-				accountTypeStream,
-				nameStream,
-				emailStream,
-				emailConfirmationStream
-			);
-
-			viewModel.stateStream.subscribe(
-				function(state) {
-					console.log("new state: '" + state + "'");
-					$scope.state = state;
-				}
-			);
-
-			viewModel.nameValidStream.subscribe(function(isValid) {
-				console.log("name is valid? " + isValid);
-				$scope.nameInvalid = !isValid;
-			});
-		});
 })();
